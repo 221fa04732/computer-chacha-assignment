1ebfdadceabd98ea1920da9cbf8c46fc56c5cd02
221fa04732 <221fa04732@gmail.com>
2025-08-11 23:14:06 +0530

    small bug fixed

--- components/landing.tsx
+++ components/landing.tsx
@@ -1,6 +1,6 @@
 export default function Landing(){
     return(<div className="w-11/12 bg-neutral-900 flex flex-col justify-center items-start pt-12 pb-2">
-        <div className="font-semibold">👋 Hey, I am Erica</div>
+        <div className="font-semibold text-white">👋 Hey, I am Erica</div>
         <div style={{background: "linear-gradient(175.4deg, #FEFAEE 5.26%, #FCD68B 29.9%, #F9784C 53.25%, #9C356D 72.94%)",WebkitBackgroundClip: "text",WebkitTextFillColor: "transparent"
         }} className="text-[56px] lg:text-[95px] leading-16 lg:leading-24 lg:pr-16 py-6">A YOUNG CREATIVE DESIGNER BASED IN SAN DIEGO
         </div>
