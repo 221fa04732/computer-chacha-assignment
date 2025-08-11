2a8f4104c8032c18b65d2837be396cd9b501e65f
221fa04732 <221fa04732@gmail.com>
2025-08-11 22:10:29 +0530

    landing section done

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Header from "@/components/header"
 import Dropmenu from "@/components/dropmenu"
 import { Company } from "@/components/company"
+import Landing from "@/components/landing"
 
 export default function Home(){
     return(<div className="flex flex-col justify-center items-center w-full min-h-screen">
@@ -10,8 +11,8 @@ export default function Home(){
         <div className="w-full fixed top-0 lg:hidden z-50 bg-neutral-900">
             <Dropmenu />
         </div>
-        <div className="flex flex-col justify-center items-center w-full mt-[73px]">
-            <div className="w-full min-h-96 bg-neutral-900"></div>
+        <div className="flex flex-col justify-center items-center w-full mt-[73px] bg-neutral-900">
+            <Landing />
             <Company />
             <div className="w-full min-h-96 bg-white"></div>
         </div>
---
+++ components/landing.tsx
@@ -0,0 +1,12 @@
+export default function Landing(){
+    return(<div className="w-11/12 bg-neutral-900 flex flex-col justify-center items-start pt-12">
+        <div className="font-semibold">👋 Hey, I am Erica</div>
+        <div style={{background: "linear-gradient(175.4deg, #FEFAEE 5.26%, #FCD68B 29.9%, #F9784C 53.25%, #9C356D 72.94%)",WebkitBackgroundClip: "text",WebkitTextFillColor: "transparent"
+        }} className="text-[56px] lg:text-[95px] leading-16 lg:leading-24 lg:pr-16 py-6">A YOUNG CREATIVE DESIGNER BASED IN SAN DIEGO
+        </div>
+        <div className="w-full flex gap-[17px] sm:justify-start justify-center">
+            <button className="border border-white rounded-full py-[12px] px-[24px]">MY WORKS</button>
+            <button className="bg-white text-black border border-white rounded-full py-[12px] px-[24px]">LET'S TALK</button>
+        </div>
+    </div>)
+}
